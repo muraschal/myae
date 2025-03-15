3301308d31ba5f9112e12d7af6052334ed6e8921
Marcel <marcel@marcelrapold.com>
2025-03-15 15:03:41 -0400

    Fix build: Verbessere Modul-Auflösung in Next.js-Konfiguration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './src',
+    };
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
 };
 
 module.exports = nextConfig;
